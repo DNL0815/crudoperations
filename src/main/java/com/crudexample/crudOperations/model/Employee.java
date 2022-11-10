2670297871d07a945e5318c08662a44588983f59
DNL0815 <dimitri.luck@outlook.com>
2022-11-09 20:42:15 -0500

    put,post,get

--- src/main/java/com/crudexample/crudOperations/model/Employee.java
+++ src/main/java/com/crudexample/crudOperations/model/Employee.java
@@ -20,6 +20,9 @@ public class Employee implements Serializable {
     @Column(nullable = false, updatable = false)
     private String employeeCode;
 
+    public Employee() {
+    }
+
     public Employee(Long id, String name, String email, String jobTitle, String phone, String imageUrl, String employeeCode) {
         this.id = id;
         this.name = name;
